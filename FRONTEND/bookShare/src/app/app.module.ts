35b7fc87d31a7f50335719f1a0c855678c53e2d3
Alba Mateos <mateos.alba@gmail.com>
2021-03-09 11:57:49 +0100

    Update html and css landing component, done static

--- FRONTEND/bookShare/src/app/app.module.ts
+++ FRONTEND/bookShare/src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LandingComponent } from './landing/landing.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { LandingComponent } from './components/landing/landing.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
