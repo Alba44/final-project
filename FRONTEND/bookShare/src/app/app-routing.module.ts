985e66659d6caae535b7924a3e9d8bbe432ee5ac
Alba Mateos <mateos.alba@gmail.com>
2021-03-15 17:45:49 +0100

    Add new route to Search Component

--- FRONTEND/bookShare/src/app/app-routing.module.ts
+++ FRONTEND/bookShare/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router'
 import { LandingComponent } from './landing/landing.component'
 import { DashboardComponent } from './dashboard/dashboard.component'
 import { ProfileComponent } from './profile/profile.component'
+import { SearchListComponent } from './search-list/search-list.component'
 // import { AuthGuardService as AuthGuard } from './auth-guard.service'
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'profile', component: ProfileComponent }
-  // { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] }
+  { path: 'profile', component: ProfileComponent },
+  { path: 'search', component: SearchListComponent }
 ]
 
 @NgModule({
