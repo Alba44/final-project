6ea2fa984704af347568d29a9a7c59a0fed88291
Alba Mateos <mateos.alba@gmail.com>
2021-03-24 20:49:00 +0100

    Test and refactor Dashboard

--- FRONTEND/bookShare/src/app/components/dashboard/dashboard.component.spec.ts
+++ FRONTEND/bookShare/src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,5 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { of } from 'rxjs'
+import { Book } from 'src/app/models/Book'
 import { BooksService } from 'src/app/services/books.service'
 
 import { DashboardComponent } from './dashboard.component'
@@ -7,13 +10,16 @@ import { DashboardComponent } from './dashboard.component'
 describe('DashboardComponent', () => {
   let component: DashboardComponent
   let fixture: ComponentFixture<DashboardComponent>
-  let mockBooksServ: Partial<BooksService>
+  let mockBooksServ: BooksService
+  let book: Book
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [DashboardComponent],
-      providers: [{ provide: BooksService, useValue: mockBooksServ }],
-      imports: [FormsModule, ReactiveFormsModule]
+      providers: [
+        { provide: BooksService },
+        { provide: FormBuilder }],
+      imports: [FormsModule, ReactiveFormsModule, HttpClientTestingModule]
     })
       .compileComponents()
   })
@@ -22,9 +28,37 @@ describe('DashboardComponent', () => {
     fixture = TestBed.createComponent(DashboardComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
+    mockBooksServ = TestBed.get(BooksService)
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should call getAllBooks on init', () => {
+    const spyBookServUpdate = spyOn(mockBooksServ, 'getAllBooks').and.callThrough()
+
+    component.ngOnInit()
+
+    expect(spyBookServUpdate).toHaveBeenCalled()
+  })
+
+  it('should return a book when calling getAllBooks', () => {
+    component.allBooks = mockBooksServ.books$
+
+    book = {
+      _id: 'string',
+      lender: 'string',
+      title: 'string',
+      author_name: [],
+      first_publish_year: 1,
+      covers: 'string',
+      description: 'string',
+      available: true
+    }
+    const spyServFunction = spyOn(mockBooksServ, 'getAllBooks').and.returnValue(of([book]))
+    component.ngOnInit()
+
+    expect(spyServFunction).toHaveBeenCalled()
   })
 })
