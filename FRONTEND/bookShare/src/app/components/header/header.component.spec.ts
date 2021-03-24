510fdde38d3a73df193d17905eec4652d0c12233
Alba Mateos <mateos.alba@gmail.com>
2021-03-24 20:49:16 +0100

    Test and refactor Header

--- FRONTEND/bookShare/src/app/components/header/header.component.spec.ts
+++ FRONTEND/bookShare/src/app/components/header/header.component.spec.ts
@@ -1,18 +1,23 @@
-/* eslint-disable no-undef */
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-import { Router } from '@angular/router'
+import { Router, RouterModule } from '@angular/router'
+import { RouterTestingModule } from '@angular/router/testing'
+import { LandingComponent } from '../landing/landing.component'
+import { ProfileComponent } from '../profile/profile.component'
 
 import { HeaderComponent } from './header.component'
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent
   let fixture: ComponentFixture<HeaderComponent>
-  let mockRouter: Partial<Router>
+  let router: Router
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HeaderComponent],
-      providers: [{ provide: Router, useValue: mockRouter }]
+      imports: [RouterTestingModule.withRoutes(
+        [{ path: 'landing', component: LandingComponent },
+          { path: 'profile/userId', component: ProfileComponent }]
+      ), RouterModule.forRoot([])]
     })
       .compileComponents()
   })
@@ -21,21 +26,26 @@ describe('HeaderComponent', () => {
     fixture = TestBed.createComponent(HeaderComponent)
     component = fixture.componentInstance
     fixture.detectChanges()
-  })
-
-  it('should call navigate with logout function', () => {
-    const mockRouter = {
-      navigate: jasmine.createSpy('navigate')
-    }
-    component.logout()
-
-    expect(mockRouter.navigate).toHaveBeenCalledWith(['/'])
+    router = TestBed.inject(Router)
   })
 
   it('should call navigate with navigate function', () => {
-    const spyFn = spyOn(component.router, 'navigate').and.callThrough()
+    spyOn(component, 'navigate').and.callThrough()
+
+    const navigateSpy = spyOn(router, 'navigate')
+
     component.navigate()
 
-    expect(spyFn).toHaveBeenCalled()
+    expect(navigateSpy).toHaveBeenCalled()
+  })
+
+  it('should call navigate with logout function', () => {
+    spyOn(component, 'navigate').and.callThrough()
+
+    const navigateSpy = spyOn(router, 'navigate')
+
+    component.logout()
+
+    expect(navigateSpy).toHaveBeenCalledWith(['/landing'])
   })
 })
--- FRONTEND/bookShare/src/app/components/header/header.component.ts
+++ FRONTEND/bookShare/src/app/components/header/header.component.ts
@@ -16,6 +16,6 @@ export class HeaderComponent {
 
   logout () {
     localStorage.removeItem('userInfo')
-    this.router.navigate(['/'])
+    this.router.navigate(['/landing'])
   }
 }
