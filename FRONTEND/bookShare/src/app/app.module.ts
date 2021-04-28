076e2983e4f4b92a0fb0942594c5f6639324afc3
Alba Mateos <mateos.alba@gmail.com>
2021-04-28 15:57:10 +0200

    Create service module and update appModule

--- FRONTEND/bookShare/src/app/app.module.ts
+++ FRONTEND/bookShare/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-
 import { AppRoutingModule } from './app-routing.module'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { ServicesModule } from './services/services.module'
+
 import { LandingComponent } from './components/landing/landing.component'
 import { DashboardComponent } from './components/dashboard/dashboard.component'
 import { HeaderComponent } from './components/header/header.component'
@@ -12,8 +13,8 @@ import { HttpClientModule } from '@angular/common/http'
 import { ModalComponent } from './components/modal/modal.component'
 import { SearchListComponent } from './components/search-list/search-list.component'
 import { BookDetailsComponent } from './components/book-details/book-details.component'
-import { AppComponent } from './app.component';
-import { ChatComponent } from './components/chat/chat.component';
+import { AppComponent } from './app.component'
+import { ChatComponent } from './components/chat/chat.component'
 import { ConversationComponent } from './components/chat/conversation/conversation.component'
 
 @NgModule({
@@ -35,7 +36,8 @@ import { ConversationComponent } from './components/chat/conversation/conversati
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ServicesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
